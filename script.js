52f375fb4b1f1d6ac4101a028410a8b23fafb5ad
Illyaas4Show <Illyaas123@outlook.com>
2022-07-07 14:40:16 +0100

    mod

---
+++ script.js
@@ -0,0 +1,7 @@
+let rateCard = document.querySelector('.rate');
+let thankYouCard = document.querySelector('.ty');
+
+let submit = () => {
+    rateCard.style.display = ('none');
+    thankYouCard.style.display = ('block');
+}
