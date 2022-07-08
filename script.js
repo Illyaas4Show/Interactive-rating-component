67bc191e5cbd33de54483e3e9a640d941cd7c4fe
Illyaas4Show <Illyaas123@outlook.com>
2022-07-08 12:16:48 +0100

    finished

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+// Selectors
 let rateCard = document.querySelector('.rate');
 let thankYouCard = document.querySelector('.ty');
+let submitBtn = document.querySelector('.submit');
+let ratingBtn = document.querySelectorAll('.btn');
+let number = document.querySelector('.number');
 
+
+var rating;
+
+// when the submit button is clicked, the rate card disappears and the thank you card appears
 let submit = () => {
-    rateCard.style.display = ('none');
-    thankYouCard.style.display = ('block');
-}
+    console.log(rating);
+
+    if (rating == undefined) {
+        alert('Please rate :)');
+    } else {
+        rateCard.style.display = ('none');
+        thankYouCard.style.display = ('block');
+    };
+};
+
+
+
+// function for the rating button to add the rating to the paragraph
+function rate(num) {
+    rating = num;
+    number.textContent = rating;
+};
